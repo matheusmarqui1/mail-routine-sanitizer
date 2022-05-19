ac81f4430403311c36eaf75c5c2c9f913ce54878
Matheus Marqui <matheus.701@live.com>
2022-05-19 10:51:20 -0300

    Bug fix: TextParser wouldn't parse more than one  if  wasn't initialized equals to

--- src/MailRoutine/Util/TextParser.php
+++ src/MailRoutine/Util/TextParser.php
@@ -4,9 +4,9 @@ use MailRoutine\Util\KeyValue;
 
 class TextParser{
     public static function parse(string $text, KeyValue ...$keyValues) : string {
-        $newText = "";
+        $newText = $text;
         foreach($keyValues as $keyValue)
-            $newText = str_replace('{{' . $keyValue->getKey() . '}}', $keyValue->getValue(), $text);
+            $newText = str_replace('{{' . $keyValue->getKey() . '}}', $keyValue->getValue(), $newText);
         
         return $newText; 
     }
